0bac89f4136e38e0193dde4486cfbf70e0fc7d39
Nico Morete <nicod.morete@gmail.com>
2022-09-19 16:58:37 -0300

    form contacto

---
+++ js/annia.js
@@ -0,0 +1,24 @@
+// BARRA BÚSQUEDA
+const buscar = document.getElementById ("buscar");
+buscar.addEventListener("input", ()=>{
+    console.log(buscar.value)
+});
+// FORMULARIO DE CONTACTO
+let formulario = document.getElementById("formulario");
+if(formulario){
+formulario.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const nombre = document.getElementById("nombre");
+    const apellido = document.getElementById("apellido");
+    const correoe = document.getElementById("correoe");
+    const consulta = document.getElementById("consulta");
+    console.log("Su nombre es: " + nombre.value);
+    console.log("Su apellido es: " + apellido.value);
+    console.log("Su Email es: " + correoe.value);
+    console.log("Formulario enviado, en breve nos contactaremos contigo.");
+    formulario.reset();
+})};
+
+const arrayLenceria = [loto, lirio];
+const arrayPijama = [amapola, aleli, dalia, girasol, ];
+const arrayAccesorios = [vincha, scrunchie, antifaz, cofia, funda];
