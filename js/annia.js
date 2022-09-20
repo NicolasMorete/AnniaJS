3075b833105e9715bb6c5e66038b4fde1bb8d12a
Nico Morete <nicod.morete@gmail.com>
2022-09-19 21:49:27 -0300

    agregar al carrito en lenceria

--- js/annia.js
+++ js/annia.js
@@ -19,6 +19,30 @@ formulario.addEventListener("submit",(e)=>{
     formulario.reset();
 })};
 
-const arrayLenceria = [loto, lirio];
-const arrayPijama = [amapola, aleli, dalia, girasol, ];
-const arrayAccesorios = [vincha, scrunchie, antifaz, cofia, funda];
+// const arrayLenceria = ["loto", "lirio"];
+// const arrayPijama = ["amapola", "aleli", "dalia", "girasol"];
+// const arrayAccesorios = ["vincha", "scrunchie", "antifaz", "cofia", "funda"];
+
+const precioloto = document.getElementById("precioloto");
+const preciocorpiñoloto = document.createElement ("p")
+preciocorpiñoloto.innerText = "2500$";
+precioloto.appendChild(preciocorpiñoloto);
+
+const preciolirio = document.getElementById("preciolirio");
+const preciocorsetlirio = document.createElement ("p");
+preciocorsetlirio.innerText = "5700$";
+preciolirio.appendChild(preciocorsetlirio);
+
+const agregarloto = document.getElementById("agregarloto")
+agregarloto.addEventListener("click", () =>{
+    console.log("Agregaste al carrito el producto Corpiño Loto");
+    console.log("Su valor es de : " + preciocorpiñoloto.innerText );
+})
+
+const agregarlirio = document.getElementById("agregarlirio")
+agregarlirio.addEventListener("click", () =>{
+    console.log("Agregaste al carrito el producto Corset Lirio");
+    console.log("Su valor es de : " + preciocorsetlirio.innerText );
+})
+
+// AGREGUÉ SOLO 2 PRODUCTOS YA QUE VOY A ESPERAR PARA PODER MODIFICAR MASIVAMENTE YA QUE CON LO VISTO HASTA AHORA NO PUEDO MODIFICAR CON ID TODOS LOS PRODUCTOS A LA VEZ
