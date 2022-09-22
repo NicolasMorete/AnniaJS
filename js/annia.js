3defac44a7478c1e20f34ec8114a8230d2a05187
Nico Morete <nicod.morete@gmail.com>
2022-09-22 17:31:42 -0300

    agregando carrito

--- js/annia.js
+++ js/annia.js
@@ -19,30 +19,160 @@ formulario.addEventListener("submit",(e)=>{
     formulario.reset();
 })};
 
-// const arrayLenceria = ["loto", "lirio"];
-// const arrayPijama = ["amapola", "aleli", "dalia", "girasol"];
-// const arrayAccesorios = ["vincha", "scrunchie", "antifaz", "cofia", "funda"];
+// const precioloto = document.getElementById("precioloto");
+// const preciocorpiñoloto = document.createElement ("p")
+// preciocorpiñoloto.innerText = "2500$";
+// precioloto.appendChild(preciocorpiñoloto);
 
-const precioloto = document.getElementById("precioloto");
-const preciocorpiñoloto = document.createElement ("p")
-preciocorpiñoloto.innerText = "2500$";
-precioloto.appendChild(preciocorpiñoloto);
+// const preciolirio = document.getElementById("preciolirio");
+// const preciocorsetlirio = document.createElement ("p");
+// preciocorsetlirio.innerText = "5700$";
+// preciolirio.appendChild(preciocorsetlirio);
 
-const preciolirio = document.getElementById("preciolirio");
-const preciocorsetlirio = document.createElement ("p");
-preciocorsetlirio.innerText = "5700$";
-preciolirio.appendChild(preciocorsetlirio);
+// const agregarloto = document.getElementById("agregarloto")
+// agregarloto.addEventListener("click", () =>{
+//     console.log("Agregaste al carrito el producto Corpiño Loto");
+//     console.log("Su valor es de : " + preciocorpiñoloto.innerText );
+// })
 
-const agregarloto = document.getElementById("agregarloto")
-agregarloto.addEventListener("click", () =>{
-    console.log("Agregaste al carrito el producto Corpiño Loto");
-    console.log("Su valor es de : " + preciocorpiñoloto.innerText );
-})
-
-const agregarlirio = document.getElementById("agregarlirio")
-agregarlirio.addEventListener("click", () =>{
-    console.log("Agregaste al carrito el producto Corset Lirio");
-    console.log("Su valor es de : " + preciocorsetlirio.innerText );
-})
+// const agregarlirio = document.getElementById("agregarlirio")
+// agregarlirio.addEventListener("click", () =>{
+//     console.log("Agregaste al carrito el producto Corset Lirio");
+//     console.log("Su valor es de : " + preciocorsetlirio.innerText );
+// })
 
 // AGREGUÉ SOLO 2 PRODUCTOS YA QUE VOY A ESPERAR PARA PODER MODIFICAR MASIVAMENTE YA QUE CON LO VISTO HASTA AHORA NO PUEDO MODIFICAR CON ID TODOS LOS PRODUCTOS A LA VEZ
+
+class Producto {
+    constructor (id, nombre,color, precio, cantidad){
+        this.id = id;
+        this.nombre = nombre;
+        this.color = color;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+}
+// CREANDO PRODUCTOS
+const corpiñoLoto = new Producto (1, "loto", "negro", 2500, 2);
+const corpiñoCatalina = new Producto (2, "catalina", "negro", 2400, 2);
+const corpiñoMagnolia = new Producto (3, "magnolia", "negro", 3000, 2);
+
+const corpiños = [corpiñoLoto, corpiñoCatalina, corpiñoMagnolia];
+
+
+const agregaralcarritoloto = document.getElementById("agregaralcarritoloto");
+agregaralcarritoloto.addEventListener("click", () => {
+        agregarAlCarrito(id);
+    })
+const carrito = [];
+
+const agregarAlCarrito = (id) => {
+    const producto = corpiños.find(producto => producto.id === id);
+    const carritoDeProducto = carrito.find(producto => producto.id === id);
+    if(carritoDeProducto) {
+        carritoDeProducto.cantidad++;
+    }else {
+        carrito.push(producto);
+    }
+    actualizarCarrito();
+}
+
+const verCarrito = document.getElementById("carritodecompra")
+verCarrito.addEventListener("click", actualizarCarrito);
+function actualizarCarrito() {
+    let aux="";
+    carrito.forEach(producto => {
+        aux += `
+                    <img src="img/${producto.id}.jpg">
+                    <div>
+                        <h3 class="card-title"> ${producto.nombre} </h3>
+                        <p class="card-text"> ${producto.precio} </p>
+                        <button onClick = "eliminarDelCarrito(${producto.id})" class="btn btn-primary"> Eliminar del Carrito </button>
+                    </div>
+                `
+    })
+
+    contenedorCarrito.innerHTML = aux;
+    calcularTotalCompra();
+}
+
+const eliminarDelCarrito = (id) => {
+    const producto = carrito.find(producto => producto.id === id);
+    carrito.splice(carrito.indexOf(producto),1);
+    actualizarCarrito();
+}
+const vaciarCarrito = document.getElementById("vaciarCarrito");
+vaciarCarrito.addEventListener("click", () => {
+    carrito.splice(0, carrito.length);
+    actualizarCarrito();
+});
+
+const totalCarrito = document.getElementById("totalCarrito");
+
+const calcularTotalCarrito = () => {
+    let total = 0; 
+    carrito.forEach( producto => {
+        total += producto.precio * producto.cantidad;
+    });
+    totalCarrito.innerHTML = total;
+}
+
+
+
+
+// // SE MUESTRA PRODUCTOS Y MODIFICAMOS DOM
+// const contenedorProductos = document.getElementById("contenedorProductos");
+
+// // BUTTON id="boton${producto.id}"
+// contenedorProductos.appendChild(spanProducto)
+
+// const boton = document.getElementById('boton$producto.id');
+// boton.addEventListener("click" , () =>{
+//     agregarAlCarrito(Producto.id);
+// }
+// )
+// // VERSIÓN SIMPLE
+// // const carrito =[];
+// // const agregarAlCarrito = (id) =>{
+// //     const Producto = corpiños.find(Producto => Producto.id === id);
+// //     carrito.push(Producto);
+// // }
+
+// const contenedorCarrito = document.getElementById("contenedorProductos");
+// const verCarrito = document.getElementById("verCarrito");
+
+// verCarrito.addEventListener("click", actualizarCarrito);
+// function actualizarCarrito () {
+//     let aux="";
+//     carrito.forEach(Producto=>{
+//         aux += ' todoo el html a mostrar producto'
+//     })
+//     contenedorCarrito.innerHTML = aux;
+// }
+
+// const agregarAlCarrito = (id) => {
+//     const producto = producto.find(producto => producto.id === id);
+//     const productoEnCarrito = 
+// }
+
+// const eliminarDelCarrito = (id) => {
+//     const producto = carito.find(producto => producto.id === id);
+//     carrito.splice(carrito.indexOf(producto),1);
+//     actualizarCarrito();
+// }
+
+// const vaciarCarrito = document.getElementById("vaciarCarrito");
+// verCarrito.addEventListener("click",() =>{
+// carrito.splice(0, carrito.length);
+// actualizarCarrito();
+// } )
+
+// const totalCompra = document.getElementById("totalCompra");
+
+// const calcularTotalCompra = () => {
+//     let total =0;
+//     carrito.forEach(producto=>{
+//         total += producto.precio * producto.cantidad;
+//         totalCompra.innerHTML
+//     })
+// }
