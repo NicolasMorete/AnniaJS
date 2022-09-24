7b77ec733c364bd688a80dcadfecc548104b417f
Nico Morete <nicod.morete@gmail.com>
2022-09-24 16:25:43 -0300

    agregando storage, intentando

--- js/annia.js
+++ js/annia.js
@@ -64,25 +64,31 @@ const agregaralcarritoloto = document.getElementById("agregaralcarritoloto");
 agregaralcarritoloto.addEventListener("click", () => {
         agregarAlCarrito(corpiñoLoto);
     })
-const carrito = [];
+
+    const carrito = [];
 
 const agregarAlCarrito = (id) => {
     const producto = corpiños.find(producto => Producto.id === id);
-    const carritoDeProducto = carrito.find(producto =>Producto.id === id);
+    const carritoDeProducto = carrito.find(producto => Producto.id === id);
     if(carritoDeProducto) {
-        carritoDeProducto.cantidad++;
+        carritoDeProducto.cantidad ++;
     }else {
-        carrito.push(producto);
+        carrito.push(Producto);
     }
     actualizarCarrito();
 }
 
 const verCarrito = document.getElementById("offcanvasRight")
 verCarrito.addEventListener("click", actualizarCarrito);
+const corpiño = JSON.parse(localStorage.getItem("Producto"));
 function actualizarCarrito() {
     let aux="";
-    carrito.forEach(producto => {
+    carrito.forEach(corpiños => {
         aux += `
+        <div class="offcanvas-header">
+      <h5 class="offcanvas-title" id="offcanvasRightLabel">Tu carrito de compras</h5>
+      <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
         <div class="card">
             <img class="imgcarrito" src="../img/nuevasesion/braletteloto2.jpg">
             <div class="">
@@ -91,9 +97,14 @@ function actualizarCarrito() {
                 <button onClick = "eliminarDelCarrito(${Producto.id})" class="btn btn-primary boton"> Eliminar del Carrito </button>
             </div>
         </div>
+        <div>
+      <h5 id="totalCarrito">Total: </h5>
+    </div>
+        <div class="offcanvas-body">
+      <button class="btn boton" id="vaciarCarrito">Vaciar Carrito</button>
+        </div>
         `;   
     })
-
     offcanvasRight.innerHTML = aux;
     calcularTotalCarrito();
 }
@@ -119,62 +130,63 @@ const calcularTotalCarrito = () => {
     totalCarrito.innerHTML = total;
 }
 
-
-
-
-// // SE MUESTRA PRODUCTOS Y MODIFICAMOS DOM
-// const contenedorProductos = document.getElementById("contenedorProductos");
-
-// // BUTTON id="boton${producto.id}"
-// contenedorProductos.appendChild(spanProducto)
-
-// const boton = document.getElementById('boton$producto.id');
-// boton.addEventListener("click" , () =>{
-//     agregarAlCarrito(Producto.id);
-// }
-// )
-// // VERSIÓN SIMPLE
-// // const carrito =[];
-// // const agregarAlCarrito = (id) =>{
-// //     const Producto = corpiños.find(Producto => Producto.id === id);
-// //     carrito.push(Producto);
-// // }
-
-// const contenedorCarrito = document.getElementById("contenedorProductos");
-// const verCarrito = document.getElementById("verCarrito");
-
-// verCarrito.addEventListener("click", actualizarCarrito);
-// function actualizarCarrito () {
-//     let aux="";
-//     carrito.forEach(Producto=>{
-//         aux += ' todoo el html a mostrar producto'
-//     })
-//     contenedorCarrito.innerHTML = aux;
-// }
-
-// const agregarAlCarrito = (id) => {
-//     const producto = producto.find(producto => producto.id === id);
-//     const productoEnCarrito = 
-// }
-
-// const eliminarDelCarrito = (id) => {
-//     const producto = carito.find(producto => producto.id === id);
-//     carrito.splice(carrito.indexOf(producto),1);
-//     actualizarCarrito();
-// }
-
-// const vaciarCarrito = document.getElementById("vaciarCarrito");
 // verCarrito.addEventListener("click",() =>{
-// carrito.splice(0, carrito.length);
-// actualizarCarrito();
-// } )
-
-// const totalCompra = document.getElementById("totalCompra");
-
-// const calcularTotalCompra = () => {
-//     let total =0;
-//     carrito.forEach(producto=>{
-//         total += producto.precio * producto.cantidad;
-//         totalCompra.innerHTML
+//     const corpi = JSON.parse(localStorage.getItem("Producto"));
+//     let aux ="";
+//     corpi.forEach(Producto => {
+//         aux+= `
+//         <div class="offcanvas-header">
+//       <h5 class="offcanvas-title" id="offcanvasRightLabel">Tu carrito de compras</h5>
+//       <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+//         </div>
+//         <div class="card">
+//             <img class="imgcarrito" src="../img/nuevasesion/braletteloto2.jpg">
+//             <div class="">
+//                 <h3 class="card-title"> ${corpiñoLoto.nombre} </h3>
+//                 <p class="card-text"> ${corpiñoLoto.precio} </p>
+//                 <button onClick = "eliminarDelCarrito(${Producto.id})" class="btn btn-primary boton"> Eliminar del Carrito </button>
+//             </div>
+//         </div>
+//         <div>
+//       <h5 id="totalCarrito">Total: </h5>
+//     </div>
+//         <div class="offcanvas-body">
+//       <button class="btn boton" id="vaciarCarrito">Vaciar Carrito</button>
+//         </div>
+//         `;
+//         offcanvasRight.innerHTML = aux;
+//         calcularTotalCarrito();
 //     })
+// })
+// const agregaralcarritolirio = document.getElementById("agregaralcarritolirio");
+// agregaralcarritolirio.addEventListener("click", () => {
+//         agregarAlCarrito(corpiñoCatalina);
+//         verCarrito.addEventListener("click", actualizarCarrito);
+// function actualizarCarrito() {
+//     let aux="";
+//     carrito.forEach(corpiños => {
+//         aux += `
+//         <div class="offcanvas-header">
+//         <h5 class="offcanvas-title" id="offcanvasRightLabel">Tu carrito de compras</h5>
+//         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+//         </div>
+//         <div class="card">
+//             <img class="imgcarrito" src="../img/nuevasesion/corset.jpg">
+//             <div class="">
+//                 <h3 class="card-title"> ${corpiñoCatalina.nombre} </h3>
+//                 <p class="card-text"> ${corpiñoCatalina.precio} </p>
+//                 <button onClick = "eliminarDelCarrito(${Producto.id})" class="btn btn-primary boton"> Eliminar del Carrito </button>
+//             </div>
+//         </div>
+//         <div>
+//         <h5 id="totalCarrito">Total: </h5>
+//     </div>
+//         <div class="offcanvas-body">
+//         <button class="btn boton" id="vaciarCarrito">Vaciar Carrito</button>
+//         </div>
+//         `;   
+//     })
+//     offcanvasRight.innerHTML = aux;
+//     calcularTotalCarrito();
 // }
+//     })
